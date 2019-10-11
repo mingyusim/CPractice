eb82fd01738d6432b203ef187c202d978fe10abd
mingyusim <mkn1123@naver.com>
2019-10-11 15:20:43 +0900

    Git Commit

--- practice12.c
+++ practice12.c
@@ -6,7 +6,8 @@ int main() {
   조건 연산자를 사용하시오 */
 
   //선언
-  int i;
+  int i, num;
+  char rst;
 
   //할당
   printf("<<양수 음수 판별기>>\n");
@@ -14,8 +15,14 @@ int main() {
   scanf("%d", &i);
 
   //연산 및 출력
-  if(i >= 0) printf("양수입니다.");
-    else  printf("음수입니다");
+  /* if(i >= 0) printf("양수입니다.");
+     else  printf("음수입니다");
+  i >= 0 ? printf("양수입니다.") : printf("음수입니다."); */
+
+  //교수님 방식
+  rst = (i >= 0) ? '+' : '-';
+
+  printf("%d는 %c입니다.\n", i, rst);
 
   return 0;
 }
